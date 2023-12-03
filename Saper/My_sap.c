759365e357b5320e522c0cab7fa79f0da207e131
Максим Алексеев <alekseev.maksim1302@gmail.com>
2023-12-03 23:06:43 +0300

    jdg

--- Saper/My_sap.c
+++ Saper/My_sap.c
@@ -4,95 +4,149 @@
 #include <conio.h>
 #include <stdbool.h>
 #include <malloc.h>
+int podch_min(int** mas, int x, int i, int j) {
 
- int prover(int** mas, int x, int y) {
-    int con = 0;
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
-            if (mas[i][j] == 0)
+    if (i > 0) {
+        if (j < x - 1) {
+            if (mas[i - 1][j + 1] == -1)
             {
-                con = con + 1;
+                mas[i][j] = mas[i][j] + 1;
+            }
+            if (mas[i - 1][j] == -1)
+            {
+                mas[i][j] = mas[i][j] + 1;
+            }
+
+        }
+        if (i < x - 1)
+        {
+            if (j > 0)
+            {
+                if (mas[i + 1][j - 1] == -1) {
+                    mas[i][j] = mas[i][j] + 1;
+                }
+            }
+            if (mas[i + 1][j] == -1) {
+                mas[i][j] = mas[i][j] + 1;
+            }
+        }
+        if (j > 0)
+        {
+            if (i > 0) {
+                if (mas[i - 1][j - 1] == -1) {
+                    mas[i][j] = mas[i][j] + 1;
+                }
+            }
+            if (mas[i][j - 1] == -1) {
+                mas[i][j] = mas[i][j] + 1;
+            }
+        }
+
+        if (j < x - 1) {
+            if (i < x - 1)
+            {
+                if (mas[i + 1][j + 1] == -1) {
+                    mas[i][j] = mas[i][j] + 1;
+                }
+            }
+            if (mas[i][j + 1] == -1) {
+                mas[i][j] = mas[i][j] + 1;
+            }
+        }
+
+
+
+    }
+}
+int pro_pro(int** mas, int x, int yt) {
+    for (int c = 0; c < x; c++)
+    {
+        for (int q = 0; c < x; c++) {
+            if (mas[c][q] != -1)
+            {
+                if (yt == 0)
+                {
+                    podch_min(mas, x, c, q);
+                    yt++;
+                }
+
             }
 
         }
     }
-    int u;
-    u = x * y;
-    if (con == u)
-    {
-        ris_pol(mas, x, y);
-        system("cls");
-        exit(1);
-        
-    }
-   
-}
 
- int atak(int** mas) {
-     int x, y;
+}
+int atak(int** mas, int razm, int yt) {
+    int x, y;
     printf_s("Укажите координату по оси x: ");
     scanf_s("%d", &x);
     printf_s("Укажите координату по оси y: ");
     scanf_s("%d", &y);
     if (mas[x][y] == -1)
-                    {
-                        mas[x][y] = 2;
-                    }
+    {
+
+        exit(1);
+    }
     else
     {
         mas[x][y] = 0;
     }
-     system("cls");
+    pro_pro(mas, razm, yt);
+    system("cls");
 }
 // рисует поле
- int ris_pol( int** mas, int x, int y) {
-     for (int c = 0; c < x; c++)
-     {
-         printf_s("\t%d",c);
-     }
-     printf_s("\n");
+int ris_pol(HANDLE hConsole, int** mas, int x, int y) {
+    for (int c = 0; c < x; c++)
+    {
+        printf_s("\t%d", c);
+    }
+    printf_s("\n");
     for (int i = 0; i < x; i++) {
 
         printf_s("%d ", i);
         for (int j = 0; j < y; j++) {
-            
-             if (mas[i][j] == 2) {
-                    printf_s("\t*");
-                    
-             }
-             else if (mas[i][j] == 0) {
+
+            /*  if (mas[i][j] == 0) {
                     printf_s("\t#");
-                    
-             }
-             else {
-                
-                    printf_s("\t.");
-                
+
+             }*/
+            if (mas[i][j] >= 0) {
+                SetConsoleTextAttribute(hConsole, 10);
+                int u;
+                u = mas[i][j];
+                printf_s("\t%d", u);
+
+            }
+            else {
+
+                printf_s("\t.");
+
             }
         }
 
         printf("\t\n");
     }
-    
+
 
 }
 int main() {
-	setlocale(LC_ALL, "ru");
-	srand((int)time(NULL));
-    int razmer,x,y;
+    setlocale(LC_ALL, "ru");
+    srand((int)time(NULL));
+    int razmer, x, y;
     int** mass;
+    HANDLE  hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     printf_s("Укажите размер поля по оси x и y: ");
     scanf_s("%d", &razmer);
     x = razmer;
     y = x;
-   mass = (int**)malloc(x * sizeof(int*));
+    mass = (int**)malloc(x * sizeof(int*));
     int  k = 0;
     // заполняем массив поля цифрами
     for (int i = 0; i < razmer; i++) {
         // Выделение памяти под хранение строк
         mass[i] = (int*)malloc(y * sizeof(int));
         for (int j = 0; j < razmer; j++) {
-                mass[i][j] = 1;
+            mass[i][j] = 1;
         }
     }
     //// заполняем массив поля минами
@@ -102,12 +156,13 @@ int main() {
         h = rand() % razmer;
         mass[p][h] = -1;
     }
+    int yt = 0;
     int yrte = 0;
-    while (yrte!=1)
+    while (yrte != 1)
     {
-        ris_pol( mass, x, y);
-        atak(mass);
-        prover(mass, x, y);
+        ris_pol(hConsole, mass, razmer, x, y);
+        atak(mass, razmer, yt);
+
     }
 
     for (int i = 0; i < razmer; i++) {
